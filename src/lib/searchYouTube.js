d8242b3ecdf5213178521612f3767c55bddd0288
apple <apple@appletekiair.lan>
2019-11-05 12:39:42 -0800

    completed search Youtube, search is needed to be done

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.get(
+    'https://www.googleapis.com/youtube/v3/search',
+    {
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      type: 'video',
+    },)
+    .done(data => {callback(data.items)})
+    .fail(data => console.log('FAILURE:', data));
 };
 
 export default searchYouTube;
